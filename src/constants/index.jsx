0010e21e600f1d0ee97e162219f03567bd834bd5
rakshith_s <rakshithsantosh@gmail.com>
2025-04-23 11:15:50 +0530

    updated information

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -12,12 +12,16 @@ import projectImage2 from "../assets/project2.jpeg";
 import projectImage3 from "../assets/project3.jpeg";
 import projectImage4 from "../assets/project4.jpeg";
 import projectImage5 from "../assets/project5.jpeg";
-import projectImage6 from "../assets/project6.jpeg";
 
 import { RiReactjsLine } from "react-icons/ri";
-import { TbBrandNextjs } from "react-icons/tb";
-import { SiMongodb } from "react-icons/si";
-import { DiRedis } from "react-icons/di";
+
+import {
+  SiExpress,
+  SiJavascript,
+  SiMongodb,
+  SiTailwindcss,
+} from "react-icons/si";
+
 import { FaNodeJs } from "react-icons/fa";
 import { BiLogoPostgresql } from "react-icons/bi";
 
@@ -27,83 +31,76 @@ export const NAVIGATION_LINKS = [
   { label: "Skills", href: "#skills" },
   { label: "Work Experience", href: "#work" },
   { label: "Education", href: "#education" },
-  { label: "Contact", href: "#contact" },
 ];
 
 export const HERO = {
-  name: "CARL FRANK",
+  name: "RAKSHITH ",
   greet: "Hello there! 👋🏻",
   description:
-    "I am a passionate frontend developer with a knack for creating beautiful and functional user interfaces. I like transforming ideas into engaging web experiences for over a decade.",
+    "I am a passionate frontend developer with a knack for creating beautiful and functional user interfaces. I like transforming ideas into engaging web experiences.",
 };
 
 export const PROJECTS = [
   {
     id: 1,
-    name: "Personal Portfolio",
+    name: "Properties Lisiting Website",
     description:
-      "A personal portfolio website built with React and Tailwind CSS to showcase my skills, projects, and contact information.",
+      "A RealEstate Website where properties can be listed,Built using expressjs reactjs mongodb nodejs and tailwindcss",
     image: projectImage1,
-    githubLink: "https://github.com/user/personal-portfolio",
+    githubLink: "https://github.com/rakshithsantosh/projectAlpha",
+    websiteLink: "https://real-estate-8o77.onrender.com",
   },
   {
     id: 2,
-    name: "E-Commerce Platform",
+    name: "Movies App",
     description:
-      "An e-commerce web application developed using Next.js and Stripe for payment integration, featuring a comprehensive product catalog, shopping cart, and checkout system.",
+      "A Movie App which fetches movie details,Built using reactjs tailwindcss and appwrite for backend",
     image: projectImage2,
-    githubLink: "https://github.com/user/ecommerce-platform",
+    githubLink: "https://github.com/rakshithsantosh/movieApp",
+    websiteLink: "https://movie-app-pi-beryl.vercel.app/",
   },
   {
     id: 3,
-    name: "Task Management Tool",
+    name: "MarkDown Previewer",
     description:
-      "A task management application built with React and Redux for efficient task tracking and team collaboration, including features like project boards, task assignment, and progress tracking.",
+      "A MarkDown Previewer website where we can preview our markdown syntax into the formatted text instantly built with vanilla javascript",
     image: projectImage3,
-    githubLink: "https://github.com/user/task-management-tool",
+    githubLink:
+      "https://github.com/rakshithsantosh/100projects/tree/main/markdownPreviewer",
+    websiteLink: "https://100projects-lilac.vercel.app/",
   },
   {
     id: 4,
-    name: "Weather App",
+    name: "MERN Authentication",
     description:
-      "A weather application that uses the OpenWeatherMap API to fetch and display current weather data and forecasts for various locations, built with React and styled-components.",
+      "A boiler plate project to start with authentication built with nodejs expressjs mongodb and reactjs for the frontend",
     image: projectImage4,
-    githubLink: "https://github.com/user/weather-app",
+    githubLink: "https://github.com/rakshithsantosh/mernAuthentication",
   },
   {
     id: 5,
-    name: "Blog Platform",
+    name: "Portfolio Website",
     description:
-      "A blogging platform developed with Next.js and Markdown for creating, editing, and publishing blog posts. It features a rich text editor, tag system, and user authentication.",
+      "A portfolio website built using reactjs tailwindcss and framer-motion",
     image: projectImage5,
-    githubLink: "https://github.com/user/blog-platform",
-  },
-  {
-    id: 6,
-    name: "Chat Application",
-    description:
-      "A real-time chat application using Firebase for backend services, including user authentication, chat rooms, and instant messaging features. Built with React and Firebase.",
-    image: projectImage6,
-    githubLink: "https://github.com/user/chat-application",
+    githubLink: "https://github.com/rakshithsantosh/devPortfolio",
   },
 ];
 
 export const BIO = [
-  "Carl Frank studied computer science at the University of Technology,from which he graduated in 2010. Following his education, he honed his craft and expertise over the course of 10 years at Tech Innovations Inc., where he immersed himself in the world of frontend development.",
-  "In 2021, driven by a shared vision for innovative web solutions and a passion for creating extraordinary user experiences Carl Frank co-founded WebArtistry alongside his creative partner, Jane Doe. Together, they set out to redefine frontend development, infusing their projects with creativity and cutting-edge technology.",
-  "As a multi-disciplinary frontend developer, Carl Frank brings a wealth of skills and expertise to his work. From his deep understanding of HTML, CSS, and JavaScript to his proficiency in modern frameworks like React and Vue.js, Carl Frank possesses a versatile skill set that allows him to craft stunning and unforgettable web experiences for his clients and audiences alike.",
+  "Experienced Front-End Developer specializing in Adobe Experience Manager (AEM) implementations, with a focus on modern JavaScript frameworks and scalable architectures. Proficient in developing dynamic and engaging user interfaces using MERN stack. Eager to expand expertise in areas such as GenAI and performance optimization techniques to deliver exceptional digital experiences.",
 ];
 
 export const SKILLS = [
   {
     icon: <RiReactjsLine className="text-4xl text-cyan-400 lg:text-5xl" />,
     name: "React",
-    experience: "2+ years",
+    experience: "2.5+ years",
   },
   {
-    icon: <TbBrandNextjs className="text-4xl text-white lg:text-5xl" />,
-    name: "Next.js",
-    experience: "1+ year",
+    icon: <SiJavascript className="text-4xl text-white lg:text-5xl" />,
+    name: "Javascript",
+    experience: "3+ years",
   },
   {
     icon: <SiMongodb className="text-4xl text-green-600 lg:text-5xl" />,
@@ -111,9 +108,9 @@ export const SKILLS = [
     experience: "1.5+ years",
   },
   {
-    icon: <DiRedis className="text-4xl text-red-600 lg:text-5xl" />,
-    name: "Redis",
-    experience: "1+ year",
+    icon: <SiTailwindcss className="text-4xl text-red-600 lg:text-5xl" />,
+    name: "TailwindCSS",
+    experience: "2+ year",
   },
   {
     icon: <FaNodeJs className="text-4xl text-green-600 lg:text-5xl" />,
@@ -121,76 +118,48 @@ export const SKILLS = [
     experience: "2+ years",
   },
   {
+    icon: <SiExpress className="text-4xl text-green-600 lg:text-5xl" />,
+    name: "Express JS",
+    experience: "2+ years",
+  },
+  {
     icon: <BiLogoPostgresql className="text-4xl text-sky-700 lg:text-5xl" />,
     name: "PostgreSQL",
-    experience: "1+ year",
+    experience: "0.5+ year",
   },
 ];
 
 export const EXPERIENCES = [
   {
-    title: "Lead Frontend Developer",
-    company: "Innovative Tech Solutions",
-    duration: "July 2020 - Present",
+    title: "Project Engineer",
+    company: "Wipro Limited",
+    duration: "July 2021 - April 2025",
     description:
-      "As the Lead Frontend Developer, I spearheaded the development of advanced web applications using cutting-edge technologies like React, Redux, and TypeScript. I worked closely with cross-functional teams, including designers, product managers, and backend developers, to deliver seamless and high-performance user experiences.",
-  },
-  {
-    title: "Frontend Engineer",
-    company: "Digital Creations",
-    duration: "February 2016 - June 2020",
-    description:
-      "At Digital Creations, I focused on building highly interactive and responsive web interfaces using HTML, CSS, JavaScript, and modern libraries like React. I collaborated closely with UX/UI designers to implement design changes that enhanced user engagement and satisfaction. My role involved optimizing website performance, ensuring cross-browser compatibility, and implementing SEO best practices. ",
-  },
-  {
-    title: "Junior Web Developer",
-    company: "Bright Future Technologies",
-    duration: "August 2014 - January 2016",
-    description:
-      "In my role as a Junior Web Developer, I assisted in the development and maintenance of various web applications. I gained hands-on experience in utilizing HTML, CSS, and JavaScript to create user-friendly interfaces. I actively participated in team meetings, contributed to project planning, and collaborated with senior developers to implement new features.",
+      "Worked as an AEM Developer for a high-traffic, US-based e-commerce platform. Developed and maintained key features using vanilla JavaScript, GraphQL, and React, leveraging Adobe Experience Manager (AEM) for content management. Collaborated within an Agile environment using Git for version control. Contributed to the improvement in LCP by implementing lazy loading.",
   },
 ];
 
 export const EDUCATION = [
   {
-    degree: "Master of Science in Computer Science",
-    institution: "Stanford University",
-    duration: "September 2012 - June 2014",
+    degree: "B Tech in Electrical Engineering",
+    institution: "PES University",
+    duration: "May 2017 - June 2021",
     description:
-      "Specialized in Human-Computer Interaction and Software Engineering. Completed a thesis on enhancing user experience in web applications through advanced interactive techniques. Participated in various projects involving frontend development, algorithms, and data structures. Graduated with honors.",
-  },
-  {
-    degree: "Bachelor of Science in Information Technology",
-    institution: "University of California, Berkeley",
-    duration: "September 2008 - June 2012",
-    description:
-      "Focused on web development, programming languages, and database management. Actively involved in coding clubs and hackathons, where I developed several web applications using HTML, CSS, JavaScript, and PHP. Completed a senior project on developing an e-commerce platform. Graduated with a high GPA.",
+      "Specialized in Embedded Engineering and Robotics. Participated in various projects involving frontend development, Participated in multiple Hackathons. Graduated with Distinction.",
   },
 ];
 
 export const SOCIAL_MEDIA_LINKS = [
   {
-    href: "https://x.com/",
-    icon: <FaFacebook fontSize={25} className="hover:opacity-80" />,
-  },
-  {
-    href: "https://x.com/",
-    icon: <FaDiscord fontSize={25} className="hover:opacity-80" />,
-  },
-  {
-    href: "https://x.com/",
-    icon: <FaInstagram fontSize={25} className="hover:opacity-80" />,
-  },
-  {
-    href: "https://x.com/",
+    href: "https://x.com/Rakshithsantos1",
     icon: <FaXTwitter fontSize={25} className="hover:opacity-80" />,
   },
   {
-    href: "https://github.com/",
+    href: "https://github.com/rakshithsantosh",
     icon: <FaGithub fontSize={25} className="hover:opacity-80" />,
   },
   {
-    href: "https://www.linkedin.com/",
+    href: "https://www.linkedin.com/in/rak-99-s/",
     icon: <FaLinkedin fontSize={25} className="hover:opacity-80" />,
   },
 ];
